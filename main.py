ddc395d9e5f30df172fc356c9ddfe911fb44b62b
Thomas Hetland <thomas.hetland@outlook.com>
2025-10-12 15:41:45 +0200

    Commit project files (exclude virtual environment)

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from GraphTsetlinMachine.graphs import Graphs
+import numpy as np
+import numba
+
 def main():
     print("Hello world")
 
 if __name__ == "__main__":
-    main()
+    graphs_train = Graphs(
+        10000, # sets how many graphs to make
+        symbols = ['A', 'B'], # symbols for each feature (XOR has 2 feature)
+        hypervector_size = 32, # how large the hypervector is to store symbols.
+        hypervector_bits = 2, # set how many bits to use to represent symbols
+    )
+
+    for graph_id in range(10000):
+        graphs_train.set_number_of_graph_nodes(graph_id, 2)
